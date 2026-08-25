936b41969778797d92358894386fcc1065386d45
htorrico <htorrico@truenorthcorporation.com>
2026-08-24 19:09:00 -0500

    ejemplo login

---
+++ WPFDemo/login.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFDemo
+{
+    /// <summary>
+    /// Interaction logic for login.xaml
+    /// </summary>
+    public partial class login : Window
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresar_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtUsuario.Text=="hugo" & txtContrasena.Password=="123456")
+            {
+                MessageBox.Show("Bienvenido al sistema");
+            }
+            else
+            {
+                MessageBox.Show("Inténtalo de nuevo");
+
+            }
+        }
+    }
+}
